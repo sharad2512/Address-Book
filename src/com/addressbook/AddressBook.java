305b10340a4623eac0d9e352a87e82fc539df117
sharad2512 <shnarute95@gmail.com>
2023-03-16 15:12:23 +0530

    ADDED : Code to create contact for address book

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,7 +1,33 @@
 package com.addressbook;
-
+import java.util.Scanner;
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welcome to address book programing");
+    Scanner scanner = new Scanner(System.in);
+    public void createContact() {
+        Contact contact = new Contact();
+        System.out.println("Enter Your First Name");
+        String firstName = scanner.next();
+        contact.setFirstname(firstName);
+        System.out.println("Enter Your Last Name");
+        String lastName = scanner.next();
+        contact.setLastname(lastName);
+        System.out.println("Enter your Address");
+        String address = scanner.next();
+        contact.setAddress(address);
+        System.out.println("Enter Your City");
+        String city = scanner.next();
+        contact.setCity(city);
+        System.out.println("Enter Your State");
+        String state = scanner.next();
+        contact.setState(state);
+        System.out.println("Enter Your Zip Code");
+        int zip = scanner.nextInt();
+        contact.setZipCode(zip);
+        System.out.println("Enter Your Phone Number");
+        long phoneNumber = scanner.nextLong();
+        contact.setPhoneNo(phoneNumber);
+        System.out.println("Enter Your Email Address");
+        String email = scanner.next();
+        contact.setEmail(email);
+        System.out.println(contact);
     }
 }
--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -1,4 +1,8 @@
 package com.addressbook;
 
 public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.createContact();
+    }
 }
